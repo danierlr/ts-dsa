e11f4e0776bdfe720f96764935f9eae89698735c
Daniel R <danieliusraudo@gmail.com>
2026-04-28 00:12:53 +0300

    add indexed heap data structure

---
+++ problems/graph/LC743-network-delay-time.test.ts
@@ -0,0 +1,15 @@
+function networkDelayTime(times: number[][], n: number, k: number): number {
+	
+}
+
+describe('tests', () => {
+  it('desc 0', () => {
+    const res = orangesRotting([
+      [2, 1, 1],
+      [1, 1, 0],
+      [0, 1, 1],
+    ])
+
+    expect(res).toEqual(4)
+  })
+})
